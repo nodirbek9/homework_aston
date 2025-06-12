4c6c657d5ffef7ca170eeae87600042a430e902e
nodirbek9 <ayitbaevnodirbek@gmail.com>
2025-06-12 22:15:21 +0300

    Fixes

--- MyDeadLock.java
+++ MyDeadLock.java
@@ -1,40 +1,46 @@
 package ru.aston.homework.MultiThread;
 
 public class MyDeadLock {
-    
-    private static final Object lock1 = new Object();
-    private static final Object lock2 = new Object();
+
+    private static final Object LOCK1 = new Object();
+    private static final Object LOCK2 = new Object();
+    private static final int SLEEP_DURATION = 100;
 
     public static void main(String[] args) {
-        Thread thread1 = new Thread(() -> {
-            synchronized (lock1) {
-                System.out.println("Поток 1 удерживает lock1");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (lock2) {
-                    System.out.println("Поток 1 удерживаетlock2");
-                }
-            }
-        });
+        Thread thread1 = createThread("Поток 1", LOCK1, LOCK2);
+        Thread thread2 = createThread("Поток 2", LOCK2, LOCK1);
 
-        Thread thread2 = new Thread(() -> {
-            synchronized (lock2) {
-                System.out.println("Поток 2 удерживает lock2");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (lock1) {
-                    System.out.println("Поток 2 удерживает lock1");
-                }
-            }
-        });
+            thread1.start();
+            thread2.start();
 
-        thread1.start();
-        thread2.start();
+            try {
+                thread1.join();
+                thread2.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+        }
+
+        private static Thread createThread(String name, Object firstLock, Object secondLock) {
+            return new Thread(() -> {
+                synchronized (firstLock) {
+                    System.out.println(name + " удерживает " + getLockName(firstLock));
+                    try {
+                        Thread.sleep(SLEEP_DURATION);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        e.printStackTrace();
+                    }
+                    synchronized (secondLock) {
+                        System.out.println(name + " удерживает " + getLockName(secondLock));
+                    }
+                }
+            });
+        }
+    private static String getLockName(Object lock) {
+        if (lock == LOCK1) return "LOCK1";
+        if (lock == LOCK2) return "LOCK2";
+        return "неизвестный LOCK";
     }
 }
--- ThreadExample.java
+++ ThreadExample.java
@@ -2,45 +2,42 @@ package ru.aston.homework.MultiThread;
 
 public class ThreadExample {
 
-    private static final Object lock = new Object();
-    private static boolean isThread_1_turn = true;
+    private static final Object LOCK = new Object();
+    private static boolean isThread1Turn = true;
 
     public static void main(String[] args) {
-        Thread thread1 = new Thread(() -> {
-            while (true) {
-                synchronized (lock) {
-                    while (!isThread_1_turn) {
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    System.out.println("1");
-                    isThread_1_turn = false;
-                    lock.notify();
-                }
-            }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            while (true) {
-                synchronized (lock) {
-                    while (isThread_1_turn) {
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    System.out.println("2");
-                    isThread_1_turn = true;
-                    lock.notify();
-                }
-            }
-        });
+        Thread thread1 = createThread("1", true);
+        Thread thread2 = createThread("2", false);
 
         thread1.start();
         thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+    }
+
+    private static Thread createThread(String message, boolean isFirstThread) {
+        return new Thread(() -> {
+            while (true) {
+                synchronized (LOCK) {
+                    while (isThread1Turn != isFirstThread) {
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(message);
+                    isThread1Turn = !isFirstThread;
+                    LOCK.notify();
+                }
+            }
+        });
     }
 }
